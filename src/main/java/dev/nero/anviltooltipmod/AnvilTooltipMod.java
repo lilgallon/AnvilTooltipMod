e9519f2955aa1f1f1a1842908873b99a2a86dbc4
N3ROO <swellchaser33@gmail.com>
2020-09-21 17:00:51 +0200

    base files

---
+++ src/main/java/dev/nero/anviltooltipmod/AnvilTooltipMod.java
@@ -0,0 +1,25 @@
+/*
+* Copyright (C) 2020 @N3ROO on Github (Lilian Gallon)
+* This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation; version 2. This program is distributed in the hope that it will
+* be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+* PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General
+* Public License along with this program.
+*/
+
+package dev.nero.anviltooltipmod;
+
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("anviltooltipmod")
+public class AnvilTooltipMod
+{
+    // If you need to log some stuff
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public AnvilTooltipMod() {
+
+    }
+}
